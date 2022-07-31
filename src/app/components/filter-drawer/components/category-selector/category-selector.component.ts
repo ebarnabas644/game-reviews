59bf2df5e0ed25c63dba6a5803de80a50d5dff7a
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-07-31 17:29:46 +0200

    Category selector component added

---
+++ src/app/components/filter-drawer/components/category-selector/category-selector.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppOther } from 'src/app/model/AppOther';
+import { FilterService } from 'src/app/services/filter.service';
+import { GameDataService } from 'src/app/services/game-data.service';
+
+@Component({
+  selector: 'app-category-selector',
+  templateUrl: './category-selector.component.html',
+  styleUrls: ['./category-selector.component.scss']
+})
+export class CategorySelectorComponent implements OnInit, OnDestroy {
+
+  categories!: AppOther[]
+  selectedCategories: AppOther[] = []
+
+  categoryDataSub!: Subscription
+  selectedCategorySub!: Subscription
+
+  constructor(private filterService: FilterService, private gameDataService: GameDataService) { }
+
+  ngOnInit(): void {
+    this.categoryDataSub = this.gameDataService.getGameCategories().subscribe(categories => {
+      this.categories = categories
+    })
+    this.selectedCategorySub = this.filterService.getCategories().subscribe(selected => {
+      this.selectedCategories = selected
+    })
+  }
+
+  onCategoryChange(event: any): void{
+    if(event.checked){
+      var objectToAdd: AppOther = { value: event.value }
+      this.selectedCategories.push(objectToAdd)
+    }
+    else{
+      var objectToAdd: AppOther = { value: event.value }
+      for(var i = 0; i < this.selectedCategories.length; i++){
+        if(this.selectedCategories[i].value == objectToAdd.value){
+          this.selectedCategories.splice(i,1)
+          break
+        }
+      }
+    }
+    this.filterService.updateCategories(this.selectedCategories)
+  }
+
+  ngOnDestroy(): void {
+    this.categoryDataSub.unsubscribe()
+    this.selectedCategorySub.unsubscribe()
+  }
+
+}
