effcd7eab81a2175af330d4a47b17ad47c44e8d1
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-02 15:05:53 +0200

    Back button added

--- src/app/components/navigation-drawer/navigation-drawer.component.ts
+++ src/app/components/navigation-drawer/navigation-drawer.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DesktopModeService } from 'src/app/services/desktop-mode.service';
 import { NavigationDrawerService } from 'src/app/services/navigation-drawer.service';
 import { ThemeService } from 'src/app/services/theme.service';
+import { Location } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
+import { FilterDrawerService } from 'src/app/services/filter-drawer.service';
 
 @Component({
   selector: 'app-drawer',
@@ -18,16 +22,16 @@ export class DrawerComponent implements OnInit {
   desktopMode = true
   desktopStatusSubscription!: Subscription
 
-  constructor(private drawerService: NavigationDrawerService, private desktopModeService: DesktopModeService, private themeService: ThemeService) {
+  constructor(private drawerService: NavigationDrawerService, private desktopModeService: DesktopModeService, private themeService: ThemeService, private router: Router, private location: Location, private cookieService: CookieService) {
    }
 
   ngOnInit(): void {
+    this.themeService.getDarkMode().subscribe(darkMode => this.darkMode = darkMode)
     this.desktopStatusSubscription = this.desktopModeService
     .getDesktopModeStatus()
     .subscribe(mode => {
       this.desktopMode = mode
     })
-    this.themeService.getDarkMode().subscribe(darkMode => this.darkMode = darkMode)
   }
 
   ngAfterViewInit(): void{
@@ -35,11 +39,19 @@ export class DrawerComponent implements OnInit {
   }
 
   toogleDarkMode(): void{
-    this.themeService.toggleDarkMode(!this.darkMode)
+    this.themeService.setDarkMode(!this.darkMode)
   }
 
   ngOnDestroy(): void{
     this.desktopStatusSubscription.unsubscribe()
+  }
+
+  isGameView(){
+    return this.router.url.match('^/search/.*$');
+  }
+
+  backButton(){
+    this.location.back()
   }
 
 }
