38097b69d0232195f9e40595fe51565e274c92c0
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-12 03:00:04 +0200

    Wishlist page added to router with guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 import { DetailPageComponent } from './pages/detail-page/detail-page.component';
 import { ForgotPasswordComponent } from './pages/login-page/components/forgot-password/forgot-password.component';
 import { SignInComponent } from './pages/login-page/components/sign-in/sign-in.component';
@@ -7,6 +8,7 @@ import { SignUpComponent } from './pages/login-page/components/sign-up/sign-up.c
 import { VerifyEmailComponent } from './pages/login-page/components/verify-email/verify-email.component';
 import { MainPageComponent } from './pages/main/main-page/main-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { WishlistPageComponent } from './pages/wishlist-page/wishlist-page.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
@@ -15,7 +17,8 @@ const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'verify-email-address', component: VerifyEmailComponent }
+  { path: 'verify-email-address', component: VerifyEmailComponent },
+  { path: 'wishlist', component: WishlistPageComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
