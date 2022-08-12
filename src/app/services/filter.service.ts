89a5d8536ece2c48cfec0f5ecd86420a68aeaefd
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-12 03:11:37 +0200

    Refactoring

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -3,81 +3,79 @@ import { Subject, Observable, BehaviorSubject } from 'rxjs';
 import { AppOther } from '../model/AppOther';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilterService {
-
-  private supportedLanguages = new BehaviorSubject<string>("");
+  private supportedLanguages = new BehaviorSubject<string>('');
   private genres = new BehaviorSubject<AppOther[]>([]);
   private categories = new BehaviorSubject<AppOther[]>([]);
   private min_metacritic_score = new BehaviorSubject<number>(0);
   private coming_soon = new BehaviorSubject<number>(-1);
-  private osArray = new BehaviorSubject<number[]>([-1,-1,-1]);
+  private osArray = new BehaviorSubject<number[]>([-1, -1, -1]);
 
-  public getSupportedLanguages(): Observable<string>{
-    return this.supportedLanguages.asObservable()
+  public getSupportedLanguages(): Observable<string> {
+    return this.supportedLanguages.asObservable();
   }
 
-  public updateSupportedLanguages(newAppName: string): void{
-    this.supportedLanguages.next(newAppName)
+  public updateSupportedLanguages(newAppName: string): void {
+    this.supportedLanguages.next(newAppName);
   }
 
-  public getGenres(): Observable<AppOther[]>{
-    return this.genres.asObservable()
+  public getGenres(): Observable<AppOther[]> {
+    return this.genres.asObservable();
   }
 
-  public updateGenres(newGenres: AppOther[]): void{
-    this.genres.next(newGenres)
+  public updateGenres(newGenres: AppOther[]): void {
+    this.genres.next(newGenres);
   }
 
-  public getCategories(): Observable<AppOther[]>{
-    return this.categories.asObservable()
+  public getCategories(): Observable<AppOther[]> {
+    return this.categories.asObservable();
   }
 
-  public updateCategories(newCategories: AppOther[]){
-    this.categories.next(newCategories)
+  public updateCategories(newCategories: AppOther[]) {
+    this.categories.next(newCategories);
   }
 
-  public getMinMetacriticScore(): Observable<number>{
-    return this.min_metacritic_score.asObservable()
+  public getMinMetacriticScore(): Observable<number> {
+    return this.min_metacritic_score.asObservable();
   }
 
-  public updateMinMetacriticScore(newMetacriticScore: number){
-    this.min_metacritic_score.next(newMetacriticScore)
+  public updateMinMetacriticScore(newMetacriticScore: number) {
+    this.min_metacritic_score.next(newMetacriticScore);
   }
 
-  public getComingSoon(): Observable<number>{
-    return this.coming_soon.asObservable()
+  public getComingSoon(): Observable<number> {
+    return this.coming_soon.asObservable();
   }
 
-  public updateComingSoon(newComingSoon: number){
-    this.coming_soon.next(newComingSoon)
+  public updateComingSoon(newComingSoon: number) {
+    this.coming_soon.next(newComingSoon);
   }
 
-  public getOsSelection(): Observable<number[]>{
-    return this.osArray.asObservable()
+  public getOsSelection(): Observable<number[]> {
+    return this.osArray.asObservable();
   }
 
-  public updateOsSelection(selectedArray: boolean[]){
-    let counter = 0
+  public updateOsSelection(selectedArray: boolean[]) {
+    let counter = 0;
     for (let i = 0; i < selectedArray.length; i++) {
-      if(selectedArray[i] != false){
+      if (selectedArray[i] != false) {
         break;
       }
       counter++;
     }
-    if(counter == selectedArray.length){
-      this.osArray.next([-1,-1,-1])
-    }
-    else{
-      let converted: number[] = [-1,-1,-1]
+    if (counter == selectedArray.length) {
+      this.osArray.next([-1, -1, -1]);
+    } else {
+      let converted: number[] = [-1, -1, -1];
       for (let i = 0; i < selectedArray.length; i++) {
-        converted[i] = selectedArray[i] ? 1 : -1
+        converted[i] = selectedArray[i] ? 1 : -1;
       }
-      console.log(converted)
-      this.osArray.next(converted)
+      console.log(converted);
+      this.osArray.next(converted);
     }
   }
 
-  constructor() { }
+  constructor() {}
 }
