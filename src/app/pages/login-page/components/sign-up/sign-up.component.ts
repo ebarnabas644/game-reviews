bbdba5fa7f84f765bdbbf5bd2a20c5503bdf8514
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-08-13 22:21:23 +0200

    Account management sites added

--- src/app/pages/login-page/components/sign-up/sign-up.component.ts
+++ src/app/pages/login-page/components/sign-up/sign-up.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/services/auth.service';
 
+@UntilDestroy()
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(
+    public authService: AuthService,
+    public translate: TranslateService
+  ) {}
 
-  constructor(public authService: AuthService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  checkLogin(email: string, pass: string, again: string) {
+    if (pass == again) {
+      this.authService.SignUp(email, pass);
+    } else {
+      let label = this.translate.instant('password-not-match');
+      window.alert(label);
+    }
   }
-
 }
